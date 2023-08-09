f9396c7210d0ce639f0dc375bd80ae53391583d3
mehdii789 <mehdisamy95@gmail.com>
2023-08-09 18:38:32 +0200

    sécurisation

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,19 @@ const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const helmet = require('helmet');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MY_PORT = process.env.PORT || '4200' ;
+const MY_APP_SECRET = process.env.APP_SECRET;
+
+app.get("/", (req, res) => {
+  return res.send(MY_APP_SECRET);
+});
+
+app.listen(MY_PORT, () => console.log(`Server running on port ${MY_PORT}`));
+
 
 
 mongoose.connect('mongodb+srv://user1:1234@cluster0.ql2j3yc.mongodb.net/?retryWrites=true&w=majority',
@@ -24,5 +37,6 @@ app.use((req, res, next) => {
   app.use('/api/sauces', sauceRoutes);
   app.use('/api/auth', userRoutes);
   app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use(helmet());
 
   module.exports = app;
